a86ec002512df114fb68b35353cdc4802e42d036
smryhane <samuli.m.ryhanen@student.jyu.fi>
2022-03-02 11:04:13 +0200

    Few changes

--- index.js
+++ index.js
@@ -4,12 +4,10 @@
 exports.calculate_sum = calculate_sum;
 exports.check_prime = check_prime;
 
-
 const { application } = require('express');
 const express = require('express');
 const app = express();
 const PORT = 8080;
-
 
 app.use(express.json() )
 
@@ -20,30 +18,37 @@ app.listen(
 
 
 app.get('/myapi', function (req, res) {
-    let action = req.query.action;
-    if(action.localeCompare('sumandcheck') === 0){
-        let numbers = req.query.numbers;
-        let sum = calculate_sum(numbers);  //parse metodi tapahtuu calculate function sisällä, joten sitä ei erikseen kutsuta tässä.
-        let is_prime = check_prime(sum);    
+    const action = req.query.action;
+    try{
+        if(action.localeCompare('sumandcheck') === 0){
+            const numbers = req.query.numbers;
+            const sum = calculate_sum(numbers);  //parse metodi tapahtuu calculate function sisällä, joten sitä ei erikseen kutsuta tässä.
+            const is_prime = check_prime(sum);    
+        
+            res.send({
+                number: sum,
+                isPrime: is_prime
+            });
+        }
+        if(action.localeCompare('checkprime') === 0){
+            const number = "0";
+            const number_list = parse_integers(req.query.number); // Jos syöte on muotoa  " 00134 1023210 3 adkaod 213"
+            for(const i of number_list){                          // Karsitaan kaikki ylimääräiset merkit ja yhdistetään jono numeroksi  
+                number +=i;
+            }        
+            const is_prime = check_prime(number);    
     
-        res.send({
-            number: sum,
-            isPrime: is_prime
+            res.send({
+                number: parseFloat(number),
+                isPrime: is_prime
+            });
+        }
+    } catch (error){
+        res.status(400).send({ 
+                invalid_query: "invalid query"
         });
-    }
-    if(action.localeCompare('checkprime') === 0){
-        let number = "0";
-        let number_list = parse_integers(req.query.number); // Jos syöte on muotoa  " 00134 1023210 3 adkaod 213"
-        for(let i of number_list){                          // Karsitaan kaikki ylimääräiset merkit ja yhdistetään jono numeroksi  
-            number +=i;
-        }        
-        let is_prime = check_prime(number);    
+    }  
 
-        res.send({
-            number: parseFloat(number),
-            isPrime: is_prime
-        });
-    }
 });
 
 /**
@@ -64,10 +69,10 @@ function calculate_sum(numbers){
     if(numbers == ""){
         return 0;
     }
-    let list = parse_integers(numbers);
-    let sum = 0;
+    const list = parse_integers(numbers);
+    const sum = 0;
     if(list.length > 0){
-        for(let i of list){
+        for(const i of list){
             sum += parseFloat(i,10);    
         }
         return sum;
@@ -80,7 +85,8 @@ function calculate_sum(numbers){
  * @returns true/false
  */
 function check_prime(num){
-    for(let i = 2, s = Math.sqrt(num); i <= s; i++)
-    if(num % i === 0) return false; 
+    for(const i = 2, s = Math.sqrt(num); i <= s; i++){
+        if(num % i === 0) return false; 
+    }
     return num > 1; 
 }
